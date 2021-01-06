ecfe2f3e2409da21a2d2d3173771834cd7b89428
Jordan Poles <jpdev.noreply@gmail.com>
2021-01-06 18:31:04 -0500

    Init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    assas
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import "@/opencv";
+const height = 400;
+const width = 400;
 
+const video = document.getElementById("videoInput"); // video is the id of video tag
+navigator.mediaDevices
+  .getUserMedia({ video: true, audio: false })
+  .then(function(stream) {
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(function(err) {
+    console.log("An error occurred! " + err);
+  });
+
+const src = new cv.Mat(height, width, cv.CV_8UC4);
+const dst = new cv.Mat(height, width, cv.CV_8UC1);
+const cap = new cv.VideoCapture(videoSource);
+const FPS = 30;
+function processVideo() {
+  const begin = Date.now();
+  cap.read(src);
+  cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+  cv.imshow("canvasOutput", dst);
+  // schedule next one.
+  const delay = 1000 / FPS - (Date.now() - begin);
+  setTimeout(processVideo, delay);
+}
+// schedule first one.
+setTimeout(processVideo, 0);
+
+import Vue from "vue";
 export default Vue.extend({
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 });
 </script>
