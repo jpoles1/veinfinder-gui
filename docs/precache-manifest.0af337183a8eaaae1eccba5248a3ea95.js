7b05021c4fea8c8b23602ce8b3575d0b6406ad0d
Jordan Poles <jpdev.noreply@gmail.com>
2021-01-13 23:31:42 -0500

    Release 0.1

---
+++ docs/precache-manifest.0af337183a8eaaae1eccba5248a3ea95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba5f13f5127f9619396",
+    "url": "/veinfinder-gui/css/chunk-vendors.7a67a5fb.css"
+  },
+  {
+    "revision": "8635441649a4dff44ae04a9fafdc0826",
+    "url": "/veinfinder-gui/index.html"
+  },
+  {
+    "revision": "5bb85c983e10a80db556",
+    "url": "/veinfinder-gui/js/app.4f0f8e6d.js"
+  },
+  {
+    "revision": "dba5f13f5127f9619396",
+    "url": "/veinfinder-gui/js/chunk-vendors.5fa97814.js"
+  },
+  {
+    "revision": "f4fc93d86b8ca95f5fc8c42f465e7ee3",
+    "url": "/veinfinder-gui/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/veinfinder-gui/robots.txt"
+  }
+]);
